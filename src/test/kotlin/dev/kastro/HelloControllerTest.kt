f36c312e8bc27098e11074780dcf0379157e9eaf
KastroWalker <victorcsa2002@gmail.com>
2022-02-14 22:31:12 -0300

    first commit

---
+++ src/test/kotlin/dev/kastro/HelloControllerTest.kt
@@ -0,0 +1,29 @@
+package dev.kastro
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class HelloControllerTest {
+    @Inject
+    @field:Client("/")
+    lateinit var client : HttpClient
+
+    @Test
+    fun helloWorldEndpointRespondsWithProperContent() {
+        val response = client.toBlocking().retrieve("/hello")
+        assertEquals("Hello World", response)
+    }
+
+    @Test
+    fun helloWorldEndpointRespondsWithProperStatusCodeAndContent() {
+        val response = client.toBlocking().exchange("/hello", String::class.java)
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals("Hello World", response.body.get())
+    }
+}
